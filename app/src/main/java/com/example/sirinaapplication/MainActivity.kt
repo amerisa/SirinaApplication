119f71c570bfb165bd7a7c499797d60dbd6c72c9
Amerisa <amerisa3@yahoo.gr>
2023-03-11 16:05:30 +0200

    change colors, icons and add button

--- app/src/main/java/com/example/sirinaapplication/MainActivity.kt
+++ app/src/main/java/com/example/sirinaapplication/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.sirinaapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sirinaapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setButtonAction()
+    }
+
+    private fun setButtonAction() {
+        binding.koubi.setOnClickListener {
+            binding.spiderella.text = "spiderelitsa"
+        }
     }
 }
